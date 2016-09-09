8580657591c6e172987f2b624ea58babec09e9e6
Edson L <ed_edsoon@hotmail.com>
2016-09-09 17:08:02 -0300

    certificado dig.

--- CertificadoDigit./cd.py
+++ CertificadoDigit./cd.py
@@ -9,33 +9,61 @@ from Crypto.PublicKey import RSA
 from Crypto import Random
 import os
 
-
+HEADER = 32 #32bytes iniciais pertence ao cabeçalho do arquivo
 #codificando
 text = "Este trabalho consiste em gerar um certificado digital válido"
 
+#codificando
+
 hashf = hashlib.sha256()
-hashf.update(text.encode('utf-8'))
-hashf.digest()
+hashf.update(text.encode(encoding = 'latin'))
+hashf = hashf.digest()
 
 rnumber = Random.new().read
 cryptKey = RSA.generate(2048, rnumber)
 publicKey = cryptKey.publickey()
-encrypted = publicKey.encrypt(hashf,32)
+encrypted = publicKey.encrypt(hashf.decode(encoding = 'latin').encode(),32)
 
-key = publicKey.exportKey().decode(encoding = 'utf-8')
-header = len(text)
+key = publicKey.exportKey('PEM').decode(encoding = 'latin')
+header = str(len(text))+ "." + str(len(encrypted[0].decode(encoding = 'latin'))) + "." +str(len(key))+"."
+if len(header) < HEADER:
+	for i in range(len(header),HEADER):
+		header+=str('#')
 
-text = text + header+ "\n...h\n" str(encrypted.decode(encoding = 'latin')) + "\nh...\n" + str(key)
+text = header + text + str(encrypted[0].decode(encoding = 'latin')) + str(key)
 
 
 #decodificando
 
-hash1 = ''
-head = "\n...h\n"
-foot = "\nh...\n"
+ReadHeader = ''
+for i in range(0,HEADER):
+	ReadHeader+=text[i]
 
-for i in text:
-	
+tSize,hashSize,pubkeySize,junk = ReadHeader.split('.')
+
+#get original text
+Readtext = ''
+#get Hash
+ReadHash = ''
+#get publickey
+ReadpubKey = ''
+for i,j in enumerate(text):
+	if i>HEADER-1 and i<int(tSize)+HEADER:
+		Readtext+=j
+	elif i>=int(tSize)+HEADER and i< int(tSize)+int(hashSize)+HEADER:
+		ReadHash+=j
+	elif i>=int(tSize)+int(hashSize)+HEADER:
+		ReadpubKey+=j
+		
+
+hashf1 = hashlib.sha256()
+hashf1.update(Readtext.encode(encoding = 'latin'))
+hashf1 = hashf1.digest()
+getKey = RSA.importKey(ReadpubKey)
+encrypted1 = getKey.encrypt(hashf1.decode(encoding = 'latin').encode(),32)
+print("Arquivo íntegro") if encrypted1[0]==ReadHash.encode('latin') else print("Arquivo com problema")
+
+
 
 
 
